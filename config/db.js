ab3047fad0cd3df170c2ff8b7d9af5fab2c3ad85
bagoesihsant <bagoesihsant@gmail.com>
2025-11-25 09:57:04 +0700

    Added dotenv to manage database credentials, and added database connector and helper.

---
+++ config/db.js
@@ -0,0 +1,26 @@
+// Import Modules
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Constants
+const currentModuleUrl = import.meta.url;
+const __filename = fileURLToPath(currentModuleUrl);
+const __dirname = dirname(__filename);
+
+// Config
+dotenv.config({ path: join(__dirname, '.env') });
+
+// Create DB Pool
+const dbCredentials = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+};
+const dbPool = Pool(dbCredentials);
+
+// Export DB Pool
+export { dbPool };
